18424d6dffc3bc38342d2a6d8b267230fa8d5ac2
theZWbutDisconnected <2090252203@qq.com>
2026-05-26 11:40:30 +0800

    Update Game.cs

--- source/Game.cs
+++ source/Game.cs
@@ -103,7 +103,7 @@ public partial class Game : Node3D
 				model.Scale = new Vector3(f4, -f4, f4);
 				model.Position = new Vector3((float)(entity.PrevX + (entity.PosX - entity.PrevX) * alpha), (float)(entity.PrevY + (entity.PosY - entity.PrevY) * alpha), (float)(entity.PrevZ + (entity.PosZ - entity.PrevZ) * alpha));
 				model.Position += new Vector3(0.0F, heightOffset, 0.0F);
-				model.RotationDegrees = new Vector3(0.0F, 180 - entity.RotYBody, 0.0F);
+				model.RotationDegrees = new Vector3(0.0F, 180 - (entity.PrevRotYBody + (entity.RotYBody - entity.PrevRotYBody) * alpha), 0.0F);
 				if (!model.IsInsideTree()) AddChild(model);
 			}
 			else
